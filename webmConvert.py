6e43b31348e8d168b6f2cace222c6e28fe3dc729
Jaden D <24631328+PoisonedPanther@users.noreply.github.com>
2019-07-02 04:30:35 -0500

    add prompt before overwriting

--- webmConvert.py
+++ webmConvert.py
@@ -14,6 +14,7 @@ CONVERT_COMMAND = FAST_CONVERT_COMMAND #SLOW_CONVERT_COMMAND for much slower but
 OUTPUT_NAME = "video.webm" #outputs to this file name in same folder as source video
 MAX_BITRATE = 2500000 #denoted in bps, default equal to 2500 kbps
 MAX_WIDTH = 1280 #for 1280x720, enforces width to keep aspect ratio
+FORCE_OVERWRITE = False #True if you want to overwrite without prompting
 
 def out(command): #wrapper to run a command in shell and return output, used to get bitrate
     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
@@ -42,7 +43,8 @@ def convert(file): #converts using specified ffmpeg command
     oldDir = os.getcwd()
     os.chdir(file[0])
     bitrate = getBitrate(file[1])
-    out(CONVERT_COMMAND.format(file[1], MAX_WIDTH, bitrate, OUTPUT_NAME)) #actual conversion step
+    if(overwriteProtection()):
+        out(CONVERT_COMMAND.format(file[1], MAX_WIDTH, bitrate, OUTPUT_NAME)) #actual conversion step
     os.chdir(oldDir)
 
 def recursiveConvert(files):
@@ -53,6 +55,16 @@ def recursiveConvert(files):
         #give user friendly output, ex. [1/43 2%] ./Tier 01 - Mic Check 1, 2/B.I - BE I
         print("[{0}/{1} {2}%] {3}".format(curr, numFiles, math.floor(curr / numFiles * 100), file[0]))
         convert(file) #do conversion
+
+def overwriteProtection(): #If output file exists, ask before overwriting
+    write = True
+    if(not FORCE_OVERWRITE and os.path.isfile(OUTPUT_NAME)): #if file exists
+        write = getBoolean("Would you like to overwrite this webm? [y/N] ")
+    return(write)
+
+def getBoolean(prompt):
+    answer = input(prompt)
+    return(answer.lower().startswith("y")) #default to no
 
 def main():
     path = getPath()
